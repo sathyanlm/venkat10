1eeb5e5092107f64bdb422592cc535cc95bc8139
venkat <venkatasykam@yahoo.com>
2016-12-17 21:34:47 +0530

    updated AddNumbers.java file with multiplication method

--- AddNumbers.java
+++ AddNumbers.java
@@ -5,6 +5,9 @@ class AddNumbers
    public int sub(int a, int b){
 	return a-b;
    }
+   public int mul(int a, int b){
+	return a*b;
+   }
    public static void main(String args[])
    {
       int x, y, z;
