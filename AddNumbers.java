a2d5cb7306bd5ba5de014bcf03b0f5bedd54a51f
venkat <venkatasykam@yahoo.com>
2016-12-18 01:55:47 +0530

    updated add menthod in AddNumbers.java file

--- AddNumbers.java
+++ AddNumbers.java
@@ -8,14 +8,7 @@ class AddNumbers
    public int mul(int a, int b){
 	return a*b;
    }
-   public static void main(String args[])
-   {
-      int x, y, z;
-      System.out.println("Enter two integers to calculate their sum ");
-      Scanner in = new Scanner(System.in);
-      x = in.nextInt();
-      y = in.nextInt();
-      z = x + y;
-      System.out.println("Sum of entered integers = "+z);
+   public int add(int a, int b){
+   {return a+b;
    }
 }
